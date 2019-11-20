ef3dabae6b3bb628262bceef76a73cf3aecb867d
Ronit <cjhacker0@gmail.com>
2019-11-20 12:21:32 +0530

    Learn increment operators

--- src/Tutorial8.java
+++ src/Tutorial8.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;  // Takes information that a user types  from a keyboard and stores it in a variable.
+
 public class Tutorial8 {
+    public static void main(String[] args) {
+        Scanner cuckoo = new Scanner(System.in);
+
+        int girls, boys, people;
+        girls = 7;
+        boys = 3;
+        people = girls % boys;
+        System.out.println(people);
+    }
 }
---
+++ src/Tutorial9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  // Takes information that a user types  from a keyboard and stores it in a variable.
+
+public class Tutorial9 {
+    public static void main(String[] args) {
+        Scanner cuckoo = new Scanner(System.in);
+
+        int tuna = 5;
+        int bass = 18;
+        // ++ is used for quickly adding one to a variable.
+        // increment operators
+        // pre incrementing adding ++ sign before a variable.
+        // post incrementing adding ++ sign after a variable.
+        System.out.println(tuna++);
+        System.out.println(tuna);
+
+    }
+}
