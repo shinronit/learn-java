0b6c39d79df6e909faaa59d820738853b023859f
Ronit <cjhacker0@gmail.com>
2019-11-21 22:11:28 +0530

    age finder

---
+++ src/Tutorial11.java
@@ -0,0 +1,15 @@
+public class Tutorial11 {
+    public static void main(String Args[]) {
+        int boy, girl;
+        boy = 18;
+        girl = 40 ;
+        //     || means or one side should be true
+        //&& both sides should be true
+        if (boy > 10 && girl < 60){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter ");
+        }
+
+    }
+}
