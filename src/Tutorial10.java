d9ea7f55e9971f3076fd2cfafb78153b9bd42d50
Ronit <cjhacker0@gmail.com>
2019-11-21 22:07:30 +0530

    Learn if statement

---
+++ src/Tutorial10.java
@@ -0,0 +1,10 @@
+public class Tutorial10 {
+    public static void  main (String Args[]){
+        int test = 6;
+        if (test == 9){
+            System.out.println("Yes");
+        }else{
+            System.out.println("Na");
+        }
+    }
+}
