c912a17731852298012abc3a4c66e374825290b9
Ronit <cjhacker0@gmail.com>
2019-11-22 15:14:30 +0530

    Getting methods from another class

---
+++ src/Tutorial14.java
@@ -0,0 +1,12 @@
+// public anyone can accees it
+// void means this method is gonna do something but its not gonna return any data
+// object name can be named anything and it will allow us to use all the stuff from another classes
+// T14tuna is our another class name
+
+public class Tutorial14 {
+    public static void main(String Args[]) {
+
+        T14tuna tunaObject = new T14tuna();
+        tunaObject.simpleMessage();
+    }
+}
