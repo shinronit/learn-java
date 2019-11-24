5ed541a78aaa54ee186bd89e5ac72f8eb25c1c30
Ronit <cjhacker0@gmail.com>
2019-11-24 23:26:55 +0530

    First friend name with constructor

---
+++ src/Tutorial17.java
@@ -0,0 +1,9 @@
+public class Tutorial17 {
+    public static void main (String [] args){
+            T17nab nabObject = new T17nab("Nab");
+            T17nab nabObject2 = new T17nab("Nab");
+            nabObject.saying();
+            nabObject2.saying();
+
+    }
+}
