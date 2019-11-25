93c4a94d90b3127cb5dd277f14b2d5cf3ddee28d
Ronit <cjhacker0@gmail.com>
2019-11-25 20:55:23 +0530

    multiple else if statements

---
+++ src/Tutorial19.java
@@ -0,0 +1,15 @@
+public class Tutorial19 {
+    public static void main(String[] args) {
+        int age = 45;
+
+        if (age >= 60) {
+            System.out.println("You are too old " + age);
+        } else if (age >= 50) {
+            System.out.println("You are in ur 50s " + age);
+        } else if (age >= 40) {
+            System.out.println("You are in ur 40s " + age);
+        } else {
+            System.out.println("You are young");
+        }
+    }
+}
