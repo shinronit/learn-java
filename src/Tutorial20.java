56503b5aeb78afeab42d27acfd406be0644811c3
Ronit <cjhacker0@gmail.com>
2019-11-25 20:56:32 +0530

    conditional operator

---
+++ src/Tutorial20.java
@@ -0,0 +1,8 @@
+// conditi0nal operator
+public class Tutorial20 {
+    public static void main (String []args){
+        int age = 51;
+        // dont forget to add colon
+        System.out.println(age > 50 ? "You are old " : "You are young" );
+    }
+}
