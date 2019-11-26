5161f5079efffcdaa28b9f3c27e0ee9a772b2fb3
Ronit <cjhacker0@gmail.com>
2019-11-26 23:38:43 +0530

    For loops

---
+++ src/Tutorial22.java
@@ -0,0 +1,12 @@
+public class Tutorial22 {
+    public static void main(String[]args) {
+        // for is built in keyword it contains 3 statements inside it ,
+        //1st where you want the loop to start
+        // 2nd where the loop to end
+        //third how much you want to increment by
+        for (int counter=6;counter<=21;counter+=3){
+            System.out.println(counter);
+        }
+
+    }
+}
