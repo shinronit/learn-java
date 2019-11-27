97477a12ba34e577b0c98843fd5d7e219841a7d0
Ronit <cjhacker0@gmail.com>
2019-11-27 23:35:26 +0530

    Built in maths methods

---
+++ src/Tutorial25.java
@@ -0,0 +1,18 @@
+public class Tutorial25 {
+    public static void main (String[]args){
+       // Positive will become negative and negative will be become positivev
+        System.out.println(Math.abs(-26.7));
+        //ceiling round up the number
+        System.out.println(Math.ceil(7.4));
+        //floor rounds down
+        System.out.println(Math.floor(7.8));
+       //max finds outs max
+        System.out.println(Math.max(8.6, 5.2));
+       //min finds outs min
+        System.out.println(Math.min(7.4, 8.2));
+        //Power
+        System.out.println(Math.pow(5,3));
+        //Square root finds the square root of a number
+        System.out.println(Math.sqrt(9));
+    }
+}
