872386fb572fe7da5b837868f996b6a0047b6e24
Ronit <cjhacker0@gmail.com>
2019-11-27 23:22:34 +0530

    Do while loop

---
+++ src/Tutorial24.java
@@ -0,0 +1,11 @@
+//do while loop
+public class Tutorial24 {
+    public static void main (String[]args){
+        int counter = 0;
+
+        do{
+            System.out.println(counter);
+            counter++;
+        }while(counter <=10);
+    }
+}
