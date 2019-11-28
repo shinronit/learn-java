8e8b37597e8b0e8a40301f98b12e79af268bc979
Ronit <cjhacker0@gmail.com>
2019-11-28 23:42:04 +0530

    arrays

---
+++ src/Tutorial27.java
@@ -0,0 +1,16 @@
+public class Tutorial27 {
+    public static void main (String [] args){
+       // array is a variable that stores multiple values in it
+       // square brackets tells java that we are using array not a normal variable
+            //int nab[]=new int [10];
+
+           // nab[0]=87;
+           // nab[1]=543;
+           // nab[9]=65;
+
+        int nab[]={2,3,4,5,6};
+
+            System.out.println(nab[2]);
+
+    }
+}
