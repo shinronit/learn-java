1355df0d96c843084e156c78c3517070206caa48
Ronit <cjhacker0@gmail.com>
2019-11-29 18:42:06 +0530

    Array Table

---
+++ src/Tutorial28.java
@@ -0,0 +1,10 @@
+public class Tutorial28 {
+        public static void main (String[]args){
+        System.out.println("Index\tValue");
+        int nab[]={32,18,12,54,21,12,542,432,53};
+
+             for (int counter=0; counter<nab.length;counter++){
+            System.out.println(counter + "\t" + nab[counter]);
+            }
+        }
+}
