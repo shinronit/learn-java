3b9e01592625da296b1c2fc3e4fecc16bb215c9d
Ronit <cjhacker0@gmail.com>
2019-11-30 21:56:14 +0530

    Int testing

---
+++ src/Inttest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Inttest {
+    public static void main (String[]args){
+        int Nab=5;
+        int Ron=5;
+        int total = Nab * Ron;
+
+        System.out.println(total);
+    }
+}
