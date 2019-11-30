90f6339b757c391a8e46cd0baf9356533a681e5e
Ronit <cjhacker0@gmail.com>
2019-11-30 20:21:44 +0530

    Enhanced For loop

---
+++ src/Tutorial31.java
@@ -0,0 +1,12 @@
+public class Tutorial31 {
+    public static void main (String[] args){
+            int nab[]={3,4,5,6,7};
+            int total=0;
+
+        for (int x: nab){
+            total+=x;
+
+        }
+        System.out.println(total);
+    }
+}
