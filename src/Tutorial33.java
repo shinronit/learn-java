141828cc93ec4705bdbc3da6eca388963a99656b
Ronit <cjhacker0@gmail.com>
2019-12-01 23:31:30 +0530

    Multidimensional Arrays

---
+++ src/Tutorial33.java
@@ -0,0 +1,6 @@
+public class Tutorial33 {
+    public static void main (String []args){
+        int firstarray[][]={{8,9,10,11},{12,13,14,15}};
+        int secondarray[][]={{30,31,32,33},{43},{4,5,6}};
+    }
+}
