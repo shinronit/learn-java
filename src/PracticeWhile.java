8ef4f02b56c58e7834b4d9094e5a8911a91d8fb1
Ronit <cjhacker0@gmail.com>
2019-12-03 23:20:53 +0530

    While loop practice

---
+++ src/PracticeWhile.java
@@ -0,0 +1,16 @@
+//while loop pratice 1
+public class PracticeWhile {
+    public static void main(String Args[]) {
+        int counter = 10;
+        while (counter < 15) {
+            System.out.println(counter);
+            counter++;
+        }
+        if (counter >15){
+            System.out.println(" Sorry this is not possible  for me " );
+        }
+        else if (counter == 15){
+            System.out.println("  I wont add any numbers now , I am tired man " );
+        }
+    }
+}
