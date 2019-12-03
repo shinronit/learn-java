c5a57bde6c7845c66ae875072883f8291689359d
Ronit <cjhacker0@gmail.com>
2019-12-03 23:46:50 +0530

    Logical Opreators

---
+++ src/LogicalOpreators.java
@@ -0,0 +1,14 @@
+public class LogicalOpreators {
+    public static void main(String Args[]) {
+        int boy, girl;
+        boy = 18;
+        girl = 32 ;
+
+        if (boy > 16 || girl > 16){
+            System.out.println("You can enter");
+        }else{
+            System.out.println("You cannot enter ");
+        }
+
+    }
+}
