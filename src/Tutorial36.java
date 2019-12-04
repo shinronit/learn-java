867f7f0d903fdbeb6b9f134e1cfa1947db2a96fd
Ronit <cjhacker0@gmail.com>
2019-12-04 22:45:42 +0530

    Time

---
+++ src/Tutorial36.java
@@ -0,0 +1,8 @@
+public class Tutorial36 {
+    public static void main (String []args){
+        T36 t36Object = new T36();
+        System.out.println(t36Object.toMilitary());
+        t36Object.setTime(13, 27, 6);
+        System.out.println(t36Object.toMilitary());
+    }
+}
