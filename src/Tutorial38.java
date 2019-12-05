84a13b7902a29797383db42910fbc9e859107b4d
Ronit <cjhacker0@gmail.com>
2019-12-05 23:46:54 +0530

    public methods

--- src/Tutorial38.java
+++ src/Tutorial38.java
@@ -1,2 +1,8 @@
 public class Tutorial38 {
+    public static void main (String []args){
+        T38Class t38Object = new T38Class();
+        System.out.println(t38Object.toMilitary());
+        t38Object.setTime(13, 27, 6);
+        System.out.println(t38Object.toMilitary());
+    }
 }
