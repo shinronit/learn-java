aaae9249e5bf4f4863c81c1a1d8c4c6a674b6867
Ronit <cjhacker0@gmail.com>
2019-12-06 20:02:02 +0530

    Variable Lengths Arguments

--- src/Tutorial35.java
+++ src/Tutorial35.java
@@ -1,9 +1,27 @@
 public class Tutorial35 {
     public static void main (String[]args){
-        System.out.println(average(43,56,73));
+        System.out.println(average(25)+"");
+
+
+
+        average(2, 3, 4);
+        int [] myArray = {2, 3, 4};
+        averageWithInputArray(myArray);
+
+        averageWithInputArray(new int [] {2, 4, 6});
+
+        average(1,2,3,4,5,6,7);
     }
 
     public static int average(int...numbers){
+        int total=0;
+        for (int x:numbers)
+            total+=x;
+
+        return total/numbers.length;
+    }
+
+    public static int averageWithInputArray(int [] numbers) {
         int total=0;
         for (int x:numbers)
             total+=x;
