218aeeb7b2e28b35be892a710088d54782a6b03c
Ronit <cjhacker0@gmail.com>
2019-12-06 20:32:03 +0530

    Class for T38

--- src/T38Class.java
+++ src/T38Class.java
@@ -1,15 +1,20 @@
 public class T38Class {
-    private int hour;
-    private int minute;
-    private int second;
+    private int hour = 1 ;
+    private int minute = 2;
+    private int second = 3;
 
-    public void setTime(int h, int m, int s){
-        hour = ((h>=0 && h<24) ? h : 0);
-        minute = ((m>=0 && m<60) ? m : 0);
-        second = ((s>=0 && s<60) ? s : 0);
+    public void setTime(int hour, int minute, int second){
+        this.hour = 13  ;
+        this.minute = 5;
+        this.second = 6;
     }
 
     public String toMilitary(){
         return String.format("%02d:%02d:%02d", hour, minute, second);
     }
+
+    public String toString(){
+        return String.format("%d:%02d:%02d %s", ((hour==0 || hour==12)?12:hour%12),minute, second,(hour < 12? "AM":"PM"));
+    }
+
 }
