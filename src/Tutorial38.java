5cd81a8a2fade546697b1c3951d661e6c2b7bc75
Ronit <cjhacker0@gmail.com>
2019-12-06 20:31:11 +0530

    This refrences

--- src/Tutorial38.java
+++ src/Tutorial38.java
@@ -2,7 +2,10 @@ public class Tutorial38 {
     public static void main (String []args){
         T38Class t38Object = new T38Class();
         System.out.println(t38Object.toMilitary());
-        t38Object.setTime(13, 27, 6);
+        System.out.println(t38Object.toString());
+
+       t38Object.setTime(13, 27, 6);
         System.out.println(t38Object.toMilitary());
+        System.out.println(t38Object.toString());
     }
 }
