998eda0db4ecdacb47626baa1fb850667b167f22
Ronit <cjhacker0@gmail.com>
2019-12-07 23:02:27 +0530

    case methods

---
+++ src/Custom.java
@@ -0,0 +1,20 @@
+public class Custom {
+    public static void main(String Args[]) {
+        int age = 3;
+
+        switch (age){
+            case 1:
+                System.out.println ("You can crawl");
+                break;
+            case 2:
+                System.out.println ("You can speak");
+                break;
+            case 3:
+                System.out.println ("You can now join the school");
+                break;
+            default:
+                System.out.println("I think you are chant ");
+        }
+
+    }
+}
