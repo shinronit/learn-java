1ac716c68c38dc332cc7e92856485a04833f5656
Ronit <cjhacker0@gmail.com>
2019-12-08 23:11:01 +0530

    Endless loop

---
+++ src/MultipleLoop.java
@@ -0,0 +1,14 @@
+public class MultipleLoop {
+    public static void main(String Args[]) {
+        int counter = 0;
+        int rigid = 0;
+        while (counter < 15) {
+            System.out.println(counter);
+            counter++;
+        }
+        while (rigid<10){
+            System.out.println(rigid);
+            rigid--;
+        }
+    }
+}
