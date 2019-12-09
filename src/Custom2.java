005048b3522492c4acd34fcacc93715a68ef0aa3
Ronit <cjhacker0@gmail.com>
2019-12-09 23:40:59 +0530

    Math Opreator

---
+++ src/Custom2.java
@@ -0,0 +1,10 @@
+public class Custom2 {
+    public static void main (String[]args){
+        System.out.println(Math.abs(-26.7));
+        System.out.println(Math.floor(7.8));
+        System.out.println(Math.max(8.6, 5.2));
+        System.out.println(Math.min(7.4, 8.2));
+        System.out.println(Math.pow(5,3));
+        System.out.println(Math.sqrt(9));
+    }
+}
