d88ad8b171e5120efc74dce33a55edca17571b32
Ronit <cjhacker0@gmail.com>
2019-12-10 23:29:14 +0530

    Else if practice

---
+++ src/Custom3.java
@@ -0,0 +1,18 @@
+public class Custom3 {
+    public static void main(String[] args) {
+        int age = 45;
+
+        if (age >= 60) {
+            System.out.println("You are " + age);
+        }
+        else if (age >= 50) {
+            System.out.println("Your age is " + age);
+        }
+        else if (age >= 40) {
+            System.out.println("Your age is " + age);
+        }
+        else {
+            System.out.println("I dont know what your age is");
+        }
+    }
+}
