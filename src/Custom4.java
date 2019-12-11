4547498e892921c19a6b57235bc01274e447b13a
Ronit <cjhacker0@gmail.com>
2019-12-11 23:03:28 +0530

    Double

---
+++ src/Custom4.java
@@ -0,0 +1,10 @@
+public class Custom4 {
+    public static void main(String args[]) {
+        double tuna = 5.25;
+
+        System.out.print("Its ");
+        System.out.print(tuna);
+        System.out.println(" $ for your movie tickets");
+        System.out.print("ENJOY");
+    }
+}
