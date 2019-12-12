473e7f356e730568c43e0fa53355845fe06774cf
Ronit <cjhacker0@gmail.com>
2019-12-12 22:54:24 +0530

    multi

---
+++ src/Custom6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Custom6 {
+    public static void main(String[] args) {
+        Scanner cuckoo = new Scanner(System.in);
+
+        int tuna = 5;
+        tuna *= 9;
+        System.out.println(tuna);
+
+    }
+}
