f06bb60b8f664477adb897de4e9f7be4acf668fc
Ronit <cjhacker0@gmail.com>
2019-12-13 16:31:18 +0530

    scanner input printer

---
+++ src/Custom7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Custom7 {
+    public static void main (String[]args){
+        System.out.println("Go ahead and type anything you want and i will repeat.");
+        Scanner Cuckoo = new Scanner(System.in);
+        System.out.println(Cuckoo.nextLine());
+    }
+}
