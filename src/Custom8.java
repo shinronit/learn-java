04a23fcdf9d3e4022a061173595829cc7e8330e6
Ronit <cjhacker0@gmail.com>
2019-12-13 16:49:09 +0530

    fruit counter

---
+++ src/Custom8.java
@@ -0,0 +1,14 @@
+public class Custom8 {
+    public static void main (String[]args){
+        int mango = 5 + 5;
+        int apple = 4 + 4;
+        int berry = 6 + 4;
+        int banana = 12 + 5;
+        int total = mango + apple + berry + banana;
+        System.out.println("Mango   =   " + mango);
+        System.out.println("Apple   =   " + apple);
+        System.out.println("Berry   =   " + berry);
+        System.out.println("banana  =   " + banana);
+        System.out.println("Total = " + total);
+    }
+}
