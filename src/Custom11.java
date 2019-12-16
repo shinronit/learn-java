878c1f130687163cb694461afd735ff571c82bc0
Ronit <cjhacker0@gmail.com>
2019-12-17 00:26:47 +0530

    Pilot Adventure

--- src/Custom11.java
+++ src/Custom11.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
 public class Custom11 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //name field code here
         System.out.println("Enter your name");
         Scanner name = new Scanner(System.in);
 
-        System.out.println("Hi " + name.next( ) + " you are stuck in an old military base choose between two doors  ");
+        System.out.println("Hi " + name.next() + " you are stuck in an old military base choose between two doors");
         System.out.print("Choose 1st door or 2nd door");
-        System.out.println("");
-        System.out.println("");
+        System.out.println();
+        System.out.println();
 
-        Scanner inputplayer = new Scanner(System.in);
-        String stage1 = inputplayer.nextLine( );
-        if ( stage1.equals("1") ) {
+        Scanner inputPlayer = new Scanner(System.in);
+        String stage1 = inputPlayer.nextLine( );
+        if (stage1.equals("1")) {
             System.out.println("You successfully came out of the basement");
             System.out.println("Oh no! guards saw me");
             System.out.println("Option 1 : Run away as fast as possible");
             System.out.println("Option 2 : Shout Help! and tell the guards you are stuck");
             System.out.println("Use 1 for option 1 and 2 for option 2 ");
-        } else if ( stage1.equals("2") ) {
+        } else if (stage1.equals("2")) {
             System.out.println("You were killed by armed soldiers ");
             System.out.println("You Died");
             System.exit(1);
@@ -29,13 +28,13 @@ public class Custom11 {
             System.exit(1);
         }
 
-        Scanner inputplayer2 = new Scanner(System.in);
-        String stage2 = inputplayer2.nextLine( );
-        if ( stage2.equals("1") ) {
-            System.out.println("Soliders thought you are a criminal and shooted you ");
+        Scanner inputPlayer2 = new Scanner(System.in);
+        String stage2 = inputPlayer2.nextLine();
+        if (stage2.equals("1")) {
+            System.out.println("Soldiers thought you are a criminal and shot you");
             System.out.println("You died");
             System.exit(1);
-        } else if ( stage2.equals("2") ) {
+        } else if (stage2.equals("2")) {
             System.out.println("They said we cannot help because some Chinese terrorists are surrounding here");
             System.out.println("So they dropped me near a shack for night stay but");
             System.out.println("You are still inside the military base");
@@ -47,14 +46,14 @@ public class Custom11 {
             System.exit(1);
         }
 
-        Scanner inputplayer3 = new Scanner(System.in);
-        String stage3 = inputplayer3.nextLine( );
-        if ( stage3.equals("1") ) {
+        Scanner inputPlayer3 = new Scanner(System.in);
+        String stage3 = inputPlayer3.nextLine();
+        if (stage3.equals("1")) {
             System.out.println("You were killed by Chinese terrorists");
             System.out.println("YOU DIED");
             System.exit(1);
-        } else if ( stage3.equals("2") ) {
-            System.out.println("Plane take off got succesfull");
+        } else if (stage3.equals("2")) {
+            System.out.println("Plane take off got successfull");
             System.out.println("You are almost out of this place");
             System.out.println("Where should I land ");
             System.out.println("The closest places are ");
@@ -65,13 +64,13 @@ public class Custom11 {
             System.exit(1);
         }
 
-        Scanner inputplayer4 = new Scanner(System.in);
-        String stage4 = inputplayer4.nextLine( );
-        if ( stage4.equals("1") ) {
+        Scanner inputPlayer4 = new Scanner(System.in);
+        String stage4 = inputPlayer4.nextLine();
+        if (stage4.equals("1")) {
             System.out.println("You were shooted by Chinese army");
             System.out.println("YOUR PLANE GOT CRASHED AND YOU ARE DEAD");
             System.exit(1);
-        } else if ( stage4.equals("2") ) {
+        } else if (stage4.equals("2")) {
             System.out.println("Looks like Airport controllers are trying to tall to me");
             System.out.println("What should I do");
             System.out.println("Option 1 : Talk to them");
@@ -81,9 +80,9 @@ public class Custom11 {
             System.exit(1);
         }
 
-        Scanner inputplayer5 = new Scanner(System.in);
-        String stage5 = inputplayer5.nextLine( );
-        if ( stage5.equals("1") ) {
+        Scanner inputPlayer5 = new Scanner(System.in);
+        String stage5 = inputPlayer5.nextLine();
+        if (stage5.equals("1")) {
             System.out.println("OK Pilot we are sending some backups for your plane landing.");
             System.out.println("You : Ok Thank you.");
             System.out.println("Controller : Please push the red button so that the wheels would be opened.");
@@ -95,7 +94,7 @@ public class Custom11 {
             System.out.println("You :  Thank you so much I will never forget this help.");
             System.out.println("Option 1 : Go out and thank them : ");
             System.out.println("Option 2 : Turn off the engine first because plane was attacked earlier by rockets when i was at the army base. ");
-        } else if ( stage5.equals("2") ) {
+        } else if (stage5.equals("2")) {
             System.out.println("Your plane got crashed because you never landed a plane. ");
             System.out.println("YOU DIED IN PLANE EXPLOSION....");
         } else {
@@ -103,9 +102,9 @@ public class Custom11 {
             System.exit(1);
         }
 
-        Scanner inputplayer6 = new Scanner(System.in);
-        String stage6 = inputplayer6.nextLine( );
-        if ( stage6.equals("1") ) {
+        Scanner inputPlayer6 = new Scanner(System.in);
+        String stage6 = inputPlayer6.nextLine();
+        if (stage6.equals("1")) {
             System.out.println("Plane got exploded....");
             System.out.println("YOU ARE DEAD....");
             System.exit(1);
@@ -113,7 +112,7 @@ public class Custom11 {
             System.out.println("I turned the engine off and now going to meet the controller.");
             System.out.println("Controller : Congrats for your safe landing.");
             System.out.println("You : Thank you so much.");
-            System.out.println("");
+            System.out.println();
             System.out.println("Congrats player for completing the game!");
             System.out.println("\uD83D\uDE38\uD83D\uDE38\uD83D\uDE38\uD83D\uDE38\uD83D\uDE38\uD83D\uDE38\uD83D\uDE38");
         } else {
