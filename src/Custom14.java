61fda86d91288539c544268adf87a780f75f6daf
Ronit <cjhacker0@gmail.com>
2019-12-18 15:44:08 +0530

    Division

---
+++ src/Custom14.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Custom14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // int num1 = 15, num2 = 2;
+        System.out.println("Enter first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second number");
+        int num2 = scanner.nextInt();
+
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+        System.out.println("Is answer correct");
+        Scanner scanner1 = new Scanner(System.in);
+        String answer = scanner1.nextLine();
+
+        if (answer.equals("yes")){
+            System.out.println("I know that i was only testing you");
+        }else if (answer.equals("no")){
+            System.out.println("Go study and stop using these programs for cheating");
+        }else{
+            System.out.println("Error you didn't wrote yes or no");
+            System.exit(0);
+        }
+    }
+}
