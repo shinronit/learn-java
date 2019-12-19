dd66916b66fe10554bbb3056debafa03c5cf92b3
Ronit <cjhacker0@gmail.com>
2019-12-19 17:05:14 +0530

    Random Score Game

--- src/Custom16.java
+++ src/Custom16.java
@@ -7,18 +7,18 @@ public class Custom16 {
         System.out.println("If you hit score 1000 you are so lucky");
         System.out.println(" Play 10 times to calculate ");
 
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
+        Random score = new Random();
+        Scanner consoleInput = new Scanner(System.in);
 
         int total = 0;
 
         for ( int counter = 1;counter <= 10;counter += 1 ) {
 
-            int oneNumber = random.nextInt(200);
-            int twoNumber = random.nextInt(110);
-            int thirdNumber = random.nextInt(100);
+            int oneNumber = score.nextInt(200);
+            int twoNumber = score.nextInt(110);
+            int thirdNumber = score.nextInt(100);
 
-            String input = scanner.nextLine();
+            String input = consoleInput.nextLine();
             if (input.equals("1")){
                 System.out.println("You Score " + oneNumber);
                 total += oneNumber;
