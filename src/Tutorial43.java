d3a7e5d3e337d7d1964282edba86b936c26cf8ca
Ronit <cjhacker0@gmail.com>
2019-12-26 15:13:55 +0530

    Compostion

---
+++ src/Tutorial43.java
@@ -0,0 +1,10 @@
+// Composition
+public class Tutorial43 {
+    public static void main (String [] args){
+        T43potpie potObject = new T43potpie(4,5,6);
+        T43 T43Object = new T43("ronit", potObject);
+
+
+        System.out.println(T43Object);
+    }
+}
