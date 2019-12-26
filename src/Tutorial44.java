a8159f86af93589efa17320d0a3c388a287de7a2
Ronit <cjhacker0@gmail.com>
2019-12-26 18:47:50 +0530

    Enum

---
+++ src/Tutorial44.java
@@ -0,0 +1,6 @@
+public class Tutorial44 {
+    public static void main (String[]args){
+        for(T44 people: T44.values())
+        System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+    }
+}
