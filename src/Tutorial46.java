024fc85ae2242e05ac91b93963525d8d7ab41e22
Ronit <cjhacker0@gmail.com>
2019-12-27 21:07:14 +0530

    Static

---
+++ src/Tutorial46.java
@@ -0,0 +1,7 @@
+public class Tutorial46 {
+    public static void main (String[]args){
+        T46 member1 = new T46("Megan","fox");
+        T46 member2 = new T46("Natalie","Portman");
+        T46 member3 = new T46("Taylor ","Swift");
+    }
+}
