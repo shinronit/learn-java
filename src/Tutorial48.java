5057b3f1164cb94a63a2ea2f0d6aa98de5b786fa
Ronit <cjhacker0@gmail.com>
2019-12-27 21:35:05 +0530

    Final

---
+++ src/Tutorial48.java
@@ -0,0 +1,10 @@
+public class Tutorial48 {
+    public static void main (String[]argss){
+        T48 T48Object = new T48(10);
+
+        for(int i = 0; i<5; i++){
+            T48Object.add();
+            System.out.printf("%s", T48Object);
+        }
+    }
+}
