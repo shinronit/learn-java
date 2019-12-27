7f14b437f0815f8613c78c8f71ac78451293d225
Ronit <cjhacker0@gmail.com>
2019-12-27 21:18:29 +0530

    More on Static

---
+++ src/Tutorial47.java
@@ -0,0 +1,10 @@
+public class Tutorial47 {
+    public static void main (String[]args){
+        T47 member1 = new T47("Megan","fox");
+        T47 member2 = new T47("Natalie","Portman");
+        T47 member3 = new T47("Taylor ","Swift");
+
+        System.out.println(T47.getMembers());
+
+    }
+}
