73584dc4fe33eb4a006273145b14934d18f88983
Ronit <cjhacker0@gmail.com>
2019-12-28 21:57:13 +0530

    Close Buttons etc

---
+++ src/Tutorial51.java
@@ -0,0 +1,17 @@
+//Inheritance is inheriting stuff from another class
+//GUI GRAPHICS USER INTERFACE
+//J option pnae class
+
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class Tutorial51 {
+    public static void main (String []args){
+
+        T51 T51 = new T51();
+        T51.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        T51.setSize(275,180);
+        T51.setVisible(true);
+
+    }
+}
