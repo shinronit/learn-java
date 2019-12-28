71d968be827f4aff161b19100c5b2b8cf855d037
Ronit <cjhacker0@gmail.com>
2019-12-28 20:28:38 +0530

    Inheritance

---
+++ src/Tutorial49.java
@@ -0,0 +1,9 @@
+//Inheritance is inheriting stuff from another class
+
+public class Tutorial49 {
+    public static void main (String []args){
+        T49 T49Object = new T49();
+
+        T49Object.eat();
+    }
+}
