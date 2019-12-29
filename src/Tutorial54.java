6d034baa0b34eb5813c14c81e525345206e19f84
Ronit <cjhacker0@gmail.com>
2019-12-29 21:13:39 +0530

    Event handler program password and name

---
+++ src/Tutorial54.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Tutorial54 {
+    public static void main (String[]args){
+
+        T54 cuckoo = new T54();
+        cuckoo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cuckoo.setSize(350,100);
+        cuckoo.setVisible(true);
+    }
+}
