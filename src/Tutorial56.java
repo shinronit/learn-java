d01f04ba40f0b5c14c04cf7d700afdb4f8a695c3
Ronit <cjhacker0@gmail.com>
2019-12-30 19:12:26 +0530

    Polymorphic Arguements

---
+++ src/Tutorial56.java
@@ -0,0 +1,11 @@
+public class Tutorial56 {
+    public static void main (String []args){
+
+        T56fatty bucky = new T56fatty();
+        T56food fo = new T56food();
+        T56food po = new T56potpie();
+
+        bucky.digest(fo);
+        bucky.digest(po);
+    }
+}
