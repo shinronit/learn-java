2092e2b0e7fe8dc897a2ee70e9d769e82c7cfa68
Ronit <cjhacker0@gmail.com>
2019-12-31 21:27:00 +0530

    ClassFileNewYear

---
+++ src/custom22Class.java
@@ -0,0 +1,11 @@
+public class custom22Class {
+    public static void NewYear(){
+        System.out.println("DELETING 2019 FILES");
+        System.out.println("DELETING CACHE & TEMP");
+        System.out.println("INSTALLING NEW YEAR");
+        System.out.println("INSTALLED");
+        System.out.println("Its is 2020 now");
+        System.out.println("HAPPY NEW YEAR");
+
+    }
+}
