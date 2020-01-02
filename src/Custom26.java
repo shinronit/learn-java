fb1609d355df8e61e38e7675ef71a1df21f28e2f
Ronit <cjhacker0@gmail.com>
2020-01-02 20:33:42 +0530

    Enum Practive

---
+++ src/Custom26.java
@@ -0,0 +1,8 @@
+import java.util.EnumSet;
+
+public class Custom26 {
+    public static void main (String []args){
+        for(Custom26class people: Custom26class.values())
+            System.out.printf("%s\t%s\t%s\n", people, people.getDesc(), people.getYear());
+    }
+}
