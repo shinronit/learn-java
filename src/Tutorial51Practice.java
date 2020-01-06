18cd6085001a06f024a725cd013e746a4cf99415
Ronit <cjhacker0@gmail.com>
2020-01-06 18:31:27 +0530

    Gui With JFrame Practice

---
+++ src/Tutorial51Practice.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class Tutorial51Practice {
+    public static void main (String []args){
+        T51 T51 = new T51();
+        T51.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        T51.setSize(275,180);
+        T51.setVisible(true);
+    }
+}
