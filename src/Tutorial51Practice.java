74c2eb26dc6f9f7f3f03ecc8c5985021ce70a6f3
Ronit <cjhacker0@gmail.com>
2020-01-06 19:13:32 +0530

    Gui with Jframe Practice

--- src/Tutorial51Practice.java
+++ src/Tutorial51Practice.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 
 public class Tutorial51Practice {
     public static void main (String []args){
-        T51 T51 = new T51();
-        T51.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        T51.setSize(275,180);
-        T51.setVisible(true);
+        T51Practice T51Practice = new T51Practice();
+        T51Practice.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        T51Practice.setSize(275,180);
+        T51Practice.setVisible(true);
     }
 }
