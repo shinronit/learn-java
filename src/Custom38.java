d1a2b655e5c1b0d4500b19d9f90f1945e9a87b30
Ronit <cjhacker0@gmail.com>
2020-01-08 20:45:30 +0530

    Random number between 360

---
+++ src/Custom38.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Custom38 {
+    public static void main (String[]aegs){
+        Random random = new Random();
+        int direction1 = random.nextInt(360 );
+            System.out.println(direction1);
+    }
+}
