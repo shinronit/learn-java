3d0c70a2ceb729a30bd43b82d379251766db3624
Ronit <cjhacker0@gmail.com>
2020-01-10 21:50:40 +0530

    Random num

---
+++ src/Custom42.java
@@ -0,0 +1,8 @@
+public class Custom42 {
+    public static void main (String [] args){
+        Custom42class custom42 = new Custom42class();
+        Custom42class2 custom42c = new Custom42class2();
+        custom42.hello();
+        custom42c.randomnum();
+    }
+}
