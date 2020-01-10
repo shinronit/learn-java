4a25e08855df913a7a1c01246a7a697dda4df061
Ronit <cjhacker0@gmail.com>
2020-01-10 21:51:20 +0530

    Random number finder class

---
+++ src/Custom42class.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Custom42class {
+    public void hello(){
+        System.out.println("HELLO THIS IS AUTO GENERATED TEXT");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
