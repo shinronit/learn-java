35302508b3266e3b8c0f2f5e846a6467c500051f
Ronit <cjhacker0@gmail.com>
2020-01-11 21:10:38 +0530

    Tricky

---
+++ src/Custom45.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Custom45 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you ready");
+        scanner.nextLine();
+        System.out.println("Do you know you cannot smile , close eyes and make your tongue out at same time");
+        scanner.nextLine();
+        System.out.println("DID YOU JUST TRIED TO DO THAT ");
+    }
+}
