22445d32e8e5843b87154d99b9ead6a5099bd329
Ronit <cjhacker0@gmail.com>
2020-01-12 23:46:03 +0530

    Table finder

---
+++ src/Custom46.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Custom46 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number between 1-9");
+            String input = scanner.nextLine();
+            int l = Integer.parseInt(input);
+            System.out.println(l*1);
+            System.out.println(l*2);
+            System.out.println(l*3);
+            System.out.println(l*4);
+            System.out.println(l*5);
+            System.out.println(l*6);
+            System.out.println(l*7);
+            System.out.println(l*8);
+            System.out.println(l*9);
+            System.out.println(l*10);
+            System.out.println("Thank you for using me.");
+    }
+}
