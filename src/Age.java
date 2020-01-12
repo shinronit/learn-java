468f1dcef47e2a9d528a9e5e4fd0e0860a12b6c2
Ronit <cjhacker0@gmail.com>
2020-01-12 23:51:27 +0530

    Age Printer

---
+++ src/Age.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String Args[]) {
+        Scanner input = new Scanner(System.in);
+        AgeClass nabObject = new AgeClass();
+        System.out.println("Enter your age :");
+        String ans = input.nextLine();
+        nabObject.setName(ans);
+        nabObject.saying();
+        System.out.println(ans);
+    }
+}
