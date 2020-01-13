efd69e62bdec069649a73c99311c04df5ee9d857
Ronit <cjhacker0@gmail.com>
2020-01-13 20:23:41 +0530

    Array Holding many Objects

---
+++ src/Tutorial60.java
@@ -0,0 +1,11 @@
+public class Tutorial60 {
+    public static void main (String [] args){
+
+        AnimalList ALO = new AnimalList();
+        Dog60 d = new Dog60();
+        Fish60 f = new Fish60();
+        ALO.add(d);
+        ALO.add(f);
+
+    }
+}
