58a1e5cf60218fa69e574ef0951e8040bb763426
Ronit <cjhacker0@gmail.com>
2020-01-14 20:40:57 +0530

    Final Check Box Program

---
+++ src/Tutorial65.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Tutorial65 {
+    public static void main (String [] args){
+
+        GUI65 go = new GUI65();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+
+    }
+}
