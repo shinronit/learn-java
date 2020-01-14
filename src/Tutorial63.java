6981d79a65a0c72f63e009a29f70ebaecaf14182
Ronit <cjhacker0@gmail.com>
2020-01-14 20:11:50 +0530

    JButton Final Program

---
+++ src/Tutorial63.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Tutorial63 {
+    public static void main (String [] args){
+
+        GUI63 go = new GUI63();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+
+    }
+}
