7b872ffd82ec17d56bf7059ce35ee35d7780094d
Ronit <cjhacker0@gmail.com>
2020-01-15 18:37:50 +0530

    JComboBox

---
+++ src/Tutorial68.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Tutorial68 {
+    public static void main (String[] args) {
+        GUI68 go = new GUI68();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+        // Continue in Tutorial69
+
+    }
+}
