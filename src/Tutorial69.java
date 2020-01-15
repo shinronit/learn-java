a726c608fd436e3129942373a632050938fb07ae
Ronit <cjhacker0@gmail.com>
2020-01-15 19:11:03 +0530

    Drop down list program

---
+++ src/Tutorial69.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Tutorial69 {
+    public static void main (String[] args) {
+        GUI69 go = new GUI69();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+
+    }
+}
