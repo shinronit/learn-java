87fc2c4283eed98898cc5788fa8376464ef71118
Ronit <cjhacker0@gmail.com>
2020-01-15 19:31:27 +0530

    JListProgram

---
+++ src/Tutorial71.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Tutorial71 {
+    public static void main (String[] args) {
+        GUI71 go = new GUI71();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
