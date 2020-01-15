93f332fcfbfa335a3e1206a8ce4bc0b2bb083d8c
Ronit <cjhacker0@gmail.com>
2020-01-15 18:18:46 +0530

    JRadioButton Final Program

---
+++ src/Tutorial67.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Tutorial67 {
+    public static void main (String [] args){
+        GUI67 go = new GUI67();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+
+    }
+}
