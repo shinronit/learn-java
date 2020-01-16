7f8bf0a6c36e2efb5f5b377cb86f4ffd68a6a147
Ronit <cjhacker0@gmail.com>
2020-01-16 19:07:58 +0530

    Moving list Items Program

---
+++ src/Tutorial73.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Tutorial73 {
+    public static void main (String[] args) {
+        GUI73 go = new GUI73();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
