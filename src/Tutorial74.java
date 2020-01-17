d72aba38e5137f6f34445a78a04ec790c3cbd524
Ronit <cjhacker0@gmail.com>
2020-01-17 20:17:11 +0530

    MouseMotionListener Interface

---
+++ src/Tutorial74.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Tutorial74 {
+    public static void main (String[] args) {
+        GUI74 go = new GUI74();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
