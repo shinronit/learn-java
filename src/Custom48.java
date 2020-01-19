c8dc47e31626acd321aae7422c3c078ef9a858e8
Ronit <cjhacker0@gmail.com>
2020-01-19 19:33:36 +0530

    Panel Color Changer GUI

---
+++ src/Custom48.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Custom48 {
+    public static void main (String[] args) {
+        customGui2 go = new customGui2();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
