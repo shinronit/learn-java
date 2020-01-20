23f9d9d42dd5bdeefacf5d458f87c77db7794297
Ronit <cjhacker0@gmail.com>
2020-01-20 22:17:23 +0530

    Font style changer gui

---
+++ src/Custom50GUI.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.logging.Handler;
+
+public class Custom50GUI extends JFrame {
+
+    private JTextField tf;
+    private Font pf;
+    private Font bf;
+    private Font itf;
+    private Font bif;
+    private JRadioButton pb;
+    private JRadioButton bb;
+    private JRadioButton ib;
+    private JRadioButton bib;
+    private ButtonGroup group;
+
+    public Custom50GUI (){
+        super("THE TITLE");
+        setLayout(new FlowLayout());
+
+        tf = new JTextField("Cuckoo  is awesome",25);
+        add(tf);
+
+        pb = new JRadioButton("Plain",true);
+        bb = new JRadioButton("Bold",false);
+        ib = new JRadioButton("Italic",false);
+        bib = new JRadioButton("Bold and Italic",false);
+        add(pb);
+        add(bb);
+        add(ib);
+        add(bib);
+
+        group = new ButtonGroup();
+        group.add(pb);
+        group.add(bb);
+        group.add(ib);
+        group.add(bib);
+
+        pf = new Font("Serif",Font.PLAIN,14);
+        bf = new Font("Serif",Font.BOLD,14);
+        itf = new Font("Serif",Font.ITALIC,14);
+        bif = new Font("Serif",Font.PLAIN + Font.ITALIC,14);
+        tf.setFont(pf);
+
+        //wait for event to happen ,pass in font object to constructor
+        pb.addItemListener(new HandlerClass(pf));
+        bb.addItemListener(new HandlerClass(bf));
+        ib.addItemListener(new HandlerClass(itf));
+        bib.addItemListener(new HandlerClass(bif));
+
+    }
+
+    private class HandlerClass implements ItemListener{
+        private Font font;
+        public HandlerClass(Font f){
+            font = f;
+        }
+        public void itemStateChanged(ItemEvent event){
+            tf.setFont(font);
+        }
+    }
+}
