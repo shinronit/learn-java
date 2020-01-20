95661da5d51a9452cf7bbc28f51373bae91a00af
Ronit <cjhacker0@gmail.com>
2020-01-20 22:16:56 +0530

    Font style changer

---
+++ src/Custom50.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Custom50 {
+    public static void main (String [] args){
+        Custom50GUI go = new Custom50GUI();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(500,350);
+        go.setVisible(true);
+
+    }
+}
