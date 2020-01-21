2b3c88bafa5b11c0b6525a2230609d74bc209aad
Ronit <cjhacker0@gmail.com>
2020-01-21 20:36:45 +0530

    MouseAdapterClass

---
+++ src/Tutorial77.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Tutorial77 {
+    public static void main (String[] args) {
+        GUI77 go = new GUI77();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
