6f0cc146444fd56f6b6f09987b84d9d19cf4ff2a
Ronit <cjhacker0@gmail.com>
2020-01-21 21:29:36 +0530

    Writing to files

---
+++ src/Tutorial80.java
@@ -0,0 +1,8 @@
+public class Tutorial80 {
+    public static void main (String [] args){
+        createFile80 g = new createFile80();
+        g.openFile();
+        g.addRecords();
+        g.closeFile();
+    }
+}
