93730172599ed7af77e2ffaa03ecea4d5aef229e
Ronit <cjhacker0@gmail.com>
2020-01-22 19:16:26 +0530

    Reading from File

---
+++ src/Tutorial81.java
@@ -0,0 +1,8 @@
+public class Tutorial81 {
+    public static void main (String [] args){
+        readFile81 g = new readFile81();
+        g.openFile();
+        g.readFile();
+        g.closeFile();
+    }
+}
