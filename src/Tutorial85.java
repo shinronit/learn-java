4562b2a94d57eb6502d9af6ddee1db3588757b41
Ronit <cjhacker0@gmail.com>
2020-01-24 18:55:24 +0530

    JColorChooser

---
+++ src/Tutorial85.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Tutorial85 {
+    public static void main (String[] args) {
+        GUI85 go = new GUI85();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300 , 200);
+        go.setVisible(true);
+    }
+}
