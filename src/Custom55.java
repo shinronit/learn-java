7cd1c89d2694f78b8d0236b3274ee1b5ac2f78bd
Ronit <cjhacker0@gmail.com>
2020-01-26 22:56:49 +0530

    Custom55.java

---
+++ src/Custom55.java
@@ -0,0 +1,10 @@
+public class Custom55 {
+    public static void main (String [] args){
+        int date = 26;
+        if(date==26){
+            System.out.println("Happy Republic Day");
+        }else{
+            System.out.println("Today date is : " + date);
+        }
+    }
+}
