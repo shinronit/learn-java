d7eb0791baa280cee7974b3ce8e6b8af800f4abe
Ronit <cjhacker0@gmail.com>
2020-01-26 22:58:36 +0530

    Happy Republic Day

--- src/Custom55.java
+++ src/Custom55.java
@@ -1,6 +1,7 @@
 public class Custom55 {
     public static void main (String [] args){
         int date = 26;
+        //Happy Republic Day
         if(date==26){
             System.out.println("Happy Republic Day");
         }else{
