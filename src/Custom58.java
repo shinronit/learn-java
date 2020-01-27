d069e36777e3fa3896f4daec35b0d5bd76a8bf0d
Ronit <cjhacker0@gmail.com>
2020-01-27 19:25:17 +0530

    Endless number loop

---
+++ src/Custom58.java
@@ -0,0 +1,8 @@
+public class Custom58 {
+    public static void main (String [] args){
+        int numbers = 1;
+        while (true){
+            System.out.println(numbers++);
+        }
+    }
+}
