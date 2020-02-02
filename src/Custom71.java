b4469187323d5883d633c8bb4f30251d941f38dc
Ronit <cjhacker0@gmail.com>
2020-02-02 20:33:41 +0530

    Scanner Print

---
+++ src/Custom71.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Custom71 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print Out What you Type");
+        System.out.println( "You typed "+ scanner.nextLine());
+    }
+}
