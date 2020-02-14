cd74af5431800bff6dadc3b739e29b865f9ef3b7
Ronit <cjhacker0@gmail.com>
2020-02-14 20:54:17 +0530

    updated cards service shinhotel

--- src/Custom89read.java
+++ src/Custom89read.java
@@ -26,4 +26,20 @@ public class Custom89read {
     public void closeFile(){
         x.close();
     }
-}
+    public static void adminPassword(){
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Enter admin email and password");
+        String adminemail = scanner.nextLine();
+        if (adminemail.equals("shin")){
+            System.out.println("password");
+            String password = scanner.nextLine();
+            if (password.equals("nihs")){
+                System.out.println("Welcome admin");
+            }else{
+                System.out.println("Wrong password");
+            }
+        }else{
+            System.out.println("Wrong email");
+        }
+}}
+
