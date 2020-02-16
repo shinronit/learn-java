2e3ea4590b50dd17dbb2c01b88046d1c43308830
Ronit <cjhacker0@gmail.com>
2020-02-16 20:06:36 +0530

    Fake fun virus

---
+++ src/Custom94.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Custom94 {
+    public static void main (String []args){
+        int i =1;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("Your console has been hacked");
+            i++;
+            System.out.println("Virus files installed " + i);
+        }
+    }
+}
