cccaeddad96a3af042bd048b94bcfafde2d30805
Ronit <cjhacker0@gmail.com>
2020-02-22 19:45:36 +0530

    !00 custom commits

---
+++ src/Custom100.java
@@ -0,0 +1,14 @@
+public class Custom100 {
+    public static void main (String [] args){
+      Custom100 c = new Custom100();
+      c.Custom100File();
+    }
+    public static void Custom100File(){
+        int filenumber = 100;
+        if (filenumber==100) {
+            System.out.println("Congrats for 100 custom commits on git-hub");
+        }else {
+            System.out.println("Do more commits ");
+        }
+    }
+}
