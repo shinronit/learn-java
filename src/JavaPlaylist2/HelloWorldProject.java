1523bdb21b6e43de6a8c1a8dd2f3223c62deaf88
Ronit <cjhacker0@gmail.com>
2020-02-28 22:53:35 +0530

    Hello worlds

---
+++ src/JavaPlaylist2/HelloWorldProject.java
@@ -0,0 +1,12 @@
+//Method is a collection of statements (one or more) that perform an operation.
+//Main method is an entry point for any java code
+//Static is a java keyword that needs an understanding of other concepts
+//void is another java keyword used to indicate that the method will not return anything
+//Statement is a complete command to be executed and cana include one or more expressions
+package JavaPlaylist2;
+
+public class HelloWorldProject {
+    public static void main (String [] args){
+        System.out.println("Helloit  World");
+    }
+}
