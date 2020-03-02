4d0707b058b066308468ee550386db79748b0588
Ronit <cjhacker0@gmail.com>
2020-03-02 21:00:51 +0530

    Variables

---
+++ src/JavaPlaylist2/Variables.java
@@ -0,0 +1,20 @@
+//Method is a collection of statements (one or more) that perform an operation.
+//Main method is an entry point for any java code
+//Static is a java keyword that needs an understanding of other concepts
+//void is another java keyword used to indicate that the method will not return anything
+//Statement is a complete command to be executed and cana include one or more expressions
+//Variables are a way to store information in our computer.They can be accessed by name
+//Declaration statement is used to define a variable by indicating the date type,and the name,and optionally to set
+//the variable to a certain value.
+
+package JavaPlaylist2;
+
+public class Variables {
+    public static void main (String [] args){
+        System.out.println("Hello  World");
+
+        int myFirstNumber = (15 + 5) + ( 2 * 10);
+        System.out.println(myFirstNumber);
+
+    }
+}
