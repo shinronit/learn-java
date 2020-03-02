337f4d56916e35a2b8fd2197a4f95cad230e3f1e
Ronit <cjhacker0@gmail.com>
2020-03-02 21:00:21 +0530

    TheCharAndBooleanPrimitiveDataTypes

---
+++ src/JavaPlaylist2/TheCharAndBooleanPrimitiveDataTypes.java
@@ -0,0 +1,12 @@
+//Char used for storing one character
+//Unicode
+
+package JavaPlaylist2;
+
+public class TheCharAndBooleanPrimitiveDataTypes {
+    public static void main (String [] args){
+
+        char myChar = 'D';
+
+    }
+}
