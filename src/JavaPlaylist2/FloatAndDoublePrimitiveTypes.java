d1157405cc52afee3d67c1924080daede86147c7
Ronit <cjhacker0@gmail.com>
2020-03-02 20:54:56 +0530

    FloatAndDouble

---
+++ src/JavaPlaylist2/FloatAndDoublePrimitiveTypes.java
@@ -0,0 +1,23 @@
+//Single occupies 32 bits
+//Double occupies 64 bits
+package JavaPlaylist2;
+
+public class FloatAndDoublePrimitiveTypes {
+    public static void main (String [] args){
+        
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        Double myMinDoubleValue = Double.MIN_VALUE;
+        Double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue =  5.25f;
+        double myDoubleValue = 5.25d;
+
+    }
+}
