db5cd43decdce78a6f5df3e0fb43b2189fc22ee4
Ronit <cjhacker0@gmail.com>
2020-03-03 21:51:30 +0530

    OperatorsOperandsAndExpressions

---
+++ src/JavaPlaylist2/OperatorsOperandsAndExpressions.java
@@ -0,0 +1,23 @@
+package JavaPlaylist2;
+
+public class OperatorsOperandsAndExpressions {
+    public static void main (String [] args){
+
+        int result = 1+2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
+    }
+}
