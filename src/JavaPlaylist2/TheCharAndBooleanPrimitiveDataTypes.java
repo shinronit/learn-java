8db08569fcf1d6d45a116b25fec933f6f94a1f04
Ronit <cjhacker0@gmail.com>
2020-03-03 21:53:37 +0530

    TheCharAndBooleanPrimitiveDataTypes

--- src/JavaPlaylist2/TheCharAndBooleanPrimitiveDataTypes.java
+++ src/JavaPlaylist2/TheCharAndBooleanPrimitiveDataTypes.java
@@ -7,6 +7,15 @@ public class TheCharAndBooleanPrimitiveDataTypes {
     public static void main (String [] args){
 
         char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrihtChar = '\u00A9';
+        System.out.println(myCopyrihtChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
 
     }
 }
