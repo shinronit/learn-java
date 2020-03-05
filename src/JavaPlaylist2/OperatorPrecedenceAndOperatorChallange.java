6ae4fbda10b56697f29d3c23cbd7d3eb555ee728
Ronit <cjhacker0@gmail.com>
2020-03-05 22:57:04 +0530

    OperatorPrecedenceAndOperatorChallange

---
+++ src/JavaPlaylist2/OperatorPrecedenceAndOperatorChallange.java
@@ -0,0 +1,17 @@
+package JavaPlaylist2;
+
+public class OperatorPrecedenceAndOperatorChallange {
+    public static void main (String [] args){
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValueTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("TheRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0 ? true : false);
+        System.out.println("isnoRemainder = " + isNoRemainder);
+        if (!isNoRemainder){
+            System.out.println("Got some remainder");
+        }
+    }
+}
