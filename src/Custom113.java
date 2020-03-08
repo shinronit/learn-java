20581d4116319aa3be6dbbd01903ab31d1b1d8a1
Ronit <cjhacker0@gmail.com>
2020-03-08 21:43:32 +0530

    Rupees convertor

--- src/Custom113.java
+++ src/Custom113.java
@@ -2,17 +2,26 @@ import java.util.Scanner;
 
 public class Custom113 {
     public static void main (String[] args) {
-        System.out.println("Compiler version 1.12");
-        for(;;) {
-            System.out.println("CONVERT INR TO USD");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-
-            float inr = Float.parseFloat(input);
-            float USD = (inr / 74.01f);
-            System.out.println(inr+" RS IS "+USD+" USD");
-            System.out.println("Enter new number");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose 1 to convert inr to usd or 2 for coverting usd to inr");
+        String inppt = scanner.nextLine();
+        if (inppt.equals("1")){
+            convert("INR", "USD", 1 / 74.01f);
+        }else if (inppt.equals("2")){
+            convert("USD", "INR", 74.01f);
         }
+    }
+
+    public static void convert(String from, String to, Float conversionFactor){
+        System.out.println("Compiler version 1.12");
+        System.out.println("CONVERT " + from + " TO " + to);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        float inr = Float.parseFloat(input);
+        float USD = (inr * conversionFactor);
+        System.out.println(inr + "" + from + " IS " + USD + to);
+        System.out.println("Enter new number");
     }
 }
 
