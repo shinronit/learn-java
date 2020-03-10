0ad7d760c77bc8da9ce2d5eedd139e9d53f8add1
Ronit <cjhacker0@gmail.com>
2020-03-10 22:27:09 +0530

    KeywordsAndExpressions

---
+++ src/JavaPlaylist2/KeywordsAndExpressions.java
@@ -0,0 +1,11 @@
+package JavaPlaylist2;
+
+public class KeywordsAndExpressions {
+    public static void main (String [ ] args){
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50){
+            System.out.println("this is an expression");
+        }
+    }
+}
