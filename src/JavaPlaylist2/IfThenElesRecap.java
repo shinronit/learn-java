c4f6949cfc88562a0fbea21236c41fd8aeccfabb
Ronit <cjhacker0@gmail.com>
2020-03-18 00:24:18 +0530

    18 march commit

--- src/JavaPlaylist2/IfThenElesRecap.java
+++ src/JavaPlaylist2/IfThenElesRecap.java
@@ -1,12 +1,14 @@
 package JavaPlaylist2;
+import java.util.Random;
 
 public class IfThenElesRecap {
     public static void main (String [] args){
-        int x = 10;
+        Random random = new Random();
+        int x =random.nextInt(11);
         if(x==1){
-
+            System.out.println("yes it is");
         }else{
-
+            System.out.println(x);
         }
     }
 }
