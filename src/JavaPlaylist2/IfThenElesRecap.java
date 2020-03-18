75018bbaa105a9bb8ba9c74d159257b3d77ea57d
Ronit <cjhacker0@gmail.com>
2020-03-19 00:24:18 +0530

    IfThenElesRecap

---
+++ src/JavaPlaylist2/IfThenElesRecap.java
@@ -0,0 +1,12 @@
+package JavaPlaylist2;
+
+public class IfThenElesRecap {
+    public static void main (String [] args){
+        int x = 10;
+        if(x==1){
+
+        }else{
+
+        }
+    }
+}
