b237dd9c15e6a3ab74e415059b0bfb64ec41d30c
Ronit <cjhacker0@gmail.com>
2020-03-22 21:19:05 +0530

    V

---
+++ src/Custom127.java
@@ -0,0 +1,8 @@
+public class Custom127 {
+    public static void main (String []args) {
+        int i = 1;
+        while (i != 3) {
+            System.out.println("It's corono time ");
+        }
+    }
+}
