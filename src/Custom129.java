ab6a11b80462f38b784266cf8721de7c250bb0ea
Ronit <cjhacker0@gmail.com>
2020-03-24 23:36:05 +0530

    kkkaifaiffdu fldnfyifnaifnairhfayefhnysdahfhqwyheafbybfaraywrfnakfbaufbdjfnyfbafiuahfudnjkyunfcjnr87fq8fje

---
+++ src/Custom129.java
@@ -0,0 +1,24 @@
+public class Custom129 {
+    public static void main (String[] args) {
+        int i = 1;
+        if (i==1){
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it is");
+            System.out.println("Ya it isit ");
+        }
+    }
+}
