a96318aee3698d895c05e8885857a423d7a73581
Ronit <cjhacker0@gmail.com>
2020-03-29 22:08:16 +0530

    hehehe

---
+++ src/Custom134.java
@@ -0,0 +1,12 @@
+public class Custom134 {
+    public static void main (String[] args) {
+        int aban = 50;
+        int tino = 100;
+        if (tino/aban==2){
+            System.out.println("NICE ");
+        }
+        else {
+            System.out.println("OOPS");
+        }
+    }
+}
