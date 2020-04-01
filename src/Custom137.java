3f6a482d94a833ec82c7c20bb8506b6347aca861
Ronit <cjhacker0@gmail.com>
2020-04-01 20:19:00 +0530

    i value testing

---
+++ src/Custom137.java
@@ -0,0 +1,11 @@
+public class Custom137 {
+    public static void main (String[] args) {
+        int i = 1000000000;
+        if (i<-1111111111){
+            System.out.println("YES");
+        }else {
+            i -= -1111111111;
+            System.out.println(i);
+        }
+    }
+}
