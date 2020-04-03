e2bd8abcf37ec031948db9e56594f3f35e9b44e1
Ronit <cjhacker0@gmail.com>
2020-04-03 23:49:03 +0530

    CodingExcercise

---
+++ src/JavaPlaylist2/CodingExcercise.java
@@ -0,0 +1,13 @@
+package JavaPlaylist2;
+
+public class CodingExcercise {
+    public static void PositiveNegativeZero(int number){
+        if (number>0){
+            System.out.println("positive");
+        }else if (number<0){
+            System.out.println("negative");
+        }else if (number==0){
+            System.out.println("zero");
+        }
+    }
+}
