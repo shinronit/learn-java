f3a53ee1e935a74e97a241f000ae4b2ce410e097
Ronit <cjhacker0@gmail.com>
2020-04-09 23:42:07 +0530

    cjac

---
+++ src/cjShinAccount.java
@@ -0,0 +1,9 @@
+public class cjShinAccount {
+    public static void main(String[] args) {
+        System.out.println("Hello CJ");
+        int coins = 20;
+        System.out.println("Coins : "+ 20);
+        int bottles = 0;
+        System.out.println("Room clean bottles " + bottles);
+    }
+}
