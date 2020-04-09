dd78af93194923cef1cd8ff74b96eaeadbe6e165
Ronit <cjhacker0@gmail.com>
2020-04-10 02:46:50 +0530

    Class file for account management of shinclanusers

---
+++ src/cjShinLoginPage.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class cjShinLoginPage {
+    public void loginpage() {
+        cjAccountOpenMain cjAccountOpenMain = new cjAccountOpenMain();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter pass : ");
+        String pass = scanner.nextLine();
+        if (pass.equals("cj@shincochan")) {
+            cjAccountOpenMain.openCjAccount();
+        }else {
+            System.out.println("Wrong pass please try again with a valid pass");
+            System.exit(0);
+        }
+    }
+}
