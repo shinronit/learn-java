d61e0a44ef310eb3cb4819a467a655c99b6eb593
Ronit <cjhacker0@gmail.com>
2020-04-18 22:54:40 +0530

    Opener

--- src/ShinAccountsManager.java
+++ src/ShinAccountsManager.java
@@ -9,9 +9,21 @@ public class ShinAccountsManager {
     public static void main(String[] args) throws NoSuchAlgorithmException{
         Scanner scanner = new Scanner(System.in);
         cjShinLoginPage cjShinLoginPage = new cjShinLoginPage();
-        System.out.println("For any issues please contact the help-line number.");
-        System.out.println("Enter Account ID or username");
-        String accountInfo = scanner.nextLine();
+        System.out.println("SHIN-CLAN USER ACCOUNT TERMINAL (V1.7.1)");
+        System.out.println("Login Signup , do you have an account ? Y/N");
+        String accountQuestion = scanner.nextLine();
+        if(accountQuestion.equalsIgnoreCase("y")){
+        }else if(accountQuestion.equalsIgnoreCase("n")){
+            System.out.println("Hi , currently we don't allow people to create there account on their own so if you want to create a account please contact the terminal admin.");
+            System.exit(0);
+        }else {
+            System.out.println("Your input : '" + accountQuestion + "' does not seems to match any command.");
+            System.out.println("Hii user , you need to type Y if you have an account if you don't have an account press N.");
+            System.out.println("Please try again with a valid input.");
+            System.exit(0);
+        }
+        System.out.print("Enter Account ID or username : ");
+        String accountInfo = scanner.next();
         if (accountInfo.equalsIgnoreCase("cj")||accountInfo.equalsIgnoreCase("51112")){
             cjShinLoginPage.loginpage();
         }else {
@@ -19,4 +31,6 @@ public class ShinAccountsManager {
             System.exit(0);
         }
     }
+
+
 }
