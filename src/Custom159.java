ba72f84ca7594d00221c964b5c95780e39f7d02f
Ronit <cjhacker0@gmail.com>
2020-04-23 23:50:55 +0530

    Hello

---
+++ src/Custom159.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Custom159 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String input = scanner.next();
+        System.out.println("Hello " + input);
+    }
+}
