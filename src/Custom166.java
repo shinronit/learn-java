754da0aba3c7da283e0f112e3e83e9e61b77d941
Ronit <cjhacker0@gmail.com>
2020-05-02 21:42:39 +0530

    Number prinnter

---
+++ src/Custom166.java
@@ -0,0 +1,9 @@
+public class Custom166 {
+    public static void main(String[] args){
+        int i = 0;
+        for (;;) {
+            System.out.println("Counting Numbers :  "+i);
+            i++;
+        }
+    }
+}
