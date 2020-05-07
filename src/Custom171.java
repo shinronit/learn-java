941ba52d685c1a1079a6156b1aa7a0536ab684ae
Ronit <cjhacker0@gmail.com>
2020-05-07 23:41:06 +0530

    Commit

---
+++ src/Custom171.java
@@ -0,0 +1,8 @@
+public class Custom171 {
+    public static void main(String[] args){
+        System.out.println("My name is PIN");
+        int pen = 121212121;
+        System.out.println("My pin code is : " + pen);
+
+    }
+}
