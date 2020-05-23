851ec52f505962378f21ea0522be063e716dc751
Ronit <cjhacker0@gmail.com>
2020-05-23 20:32:35 +0530

    Commit

---
+++ src/Custom187.java
@@ -0,0 +1,11 @@
+public class Custom187 {
+    public static void main(String[] args){
+        int i  = 20;
+        int a = 21;
+        if(i>a){
+            System.out.println(a/i);
+        }else {
+            System.out.println(a*i);
+        }
+    }
+}
