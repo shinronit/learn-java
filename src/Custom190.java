7c94fa8e02d8acdee2463867efdd2277dabcc33b
Ronit <cjhacker0@gmail.com>
2020-05-31 11:40:30 +0530

    Maths Opreators

---
+++ src/Custom190.java
@@ -0,0 +1,10 @@
+public class Custom190 {
+    public static void main (String[]args){
+        System.out.println(Math.abs(226.7));
+        System.out.println(Math.floor(17.8));
+        System.out.println(Math.max(812.621, 5.2));
+        System.out.println(Math.min(129.4, 8.2));
+        System.out.println(Math.pow(125,3));
+        System.out.println(Math.sqrt(11));
+    }
+}
