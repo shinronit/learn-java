ac35cbadefb30bea23a8c90ff9738e3c22d4486c
Ronit <cjhacker0@gmail.com>
2020-06-02 19:50:30 +0530

    While loop

---
+++ src/Custom193.java
@@ -0,0 +1,17 @@
+public class Custom193 {
+    public static void main(String Args[]) {
+        int counter = 0;
+        while (counter < 15) {
+            System.out.println(counter);
+            counter++;
+        }
+
+        if (counter > 15) {
+            System.out.println(" Sorry this is not possible  for me " );
+        }
+
+        else if (counter == 15) {
+            System.out.println("I am tired of adding numbers. Stopping Initiated.");
+        }
+    }
+}
