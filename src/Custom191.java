7ead73e585216b9b9e619aa9817339abbcf56e43
Ronit <cjhacker0@gmail.com>
2020-06-07 13:22:02 +0530

    sout practice

---
+++ src/Custom191.java
@@ -0,0 +1,11 @@
+public class Custom191 {
+    public static void main(String[] args){
+        double aban;
+        aban = 31.12;
+        System.out.print("It is ");
+        System.out.print(aban);
+        System.out.print(" $ for your ");
+        System.out.print("movie ");
+        System.out.print("tickets");
+    }
+}
