251c4314a6ae9b3317370d1cb0db9eb8fb187ae8
Ronit <cjhacker0@gmail.com>
2020-09-03 14:58:06 +0530

    Using Methods to Print String

---
+++ src/StringPrint.java
@@ -0,0 +1,8 @@
+public class StringPrint {
+    public static void main(String[] args) {
+        StringPrint("This is a test String");
+    }
+    public static void StringPrint(String userInput){
+        System.out.println(userInput);
+    }
+}
