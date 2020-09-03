b8491b695ca395120b518a019f7ed6d1204c78ff
Ronit <cjhacker0@gmail.com>
2020-09-03 21:58:11 +0530

    String to double

--- src/readingUserInput.java
+++ src/readingUserInput.java
@@ -4,16 +4,26 @@ public class readingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();//handle next line character (enter key )
+        boolean hasNextInt = scanner.hasNextInt();
+        if(hasNextInt){
+            int yearOfBirth = scanner.nextInt();
 
-        int age = 2020 - yearOfBirth;
-        System.out.println("Enter your name : ");
-        String name = scanner.nextLine();
+            scanner.nextLine();//handle next line character (enter key )
 
+            int age = 2020 - yearOfBirth;
+            System.out.println("Enter your name : ");
+            String name = scanner.nextLine();
 
+            if(age >0 && age <= 100){
+                System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            }else{
+                System.out.println("Invalid year of birth");
+            }
 
-        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+        }else{
+            System.out.println("Unable to parse year of birth");
+        }
+
 
         scanner.close();
     }
