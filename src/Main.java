fd7d0f598d4bad9f0b50606e2b1ca9eea317db88
Ronit <cjhacker0@gmail.com>
2020-09-15 19:36:06 +0530

    test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car porchse = new Car();
+
+        porchse.setModel("carrera");
+        System.out.println("Model is " + porchse.getModel());
+    }
+}
