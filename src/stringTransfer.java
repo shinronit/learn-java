d6054f30185d1b991ae9bd027e914faca8e7ec11
Ronit <cjhacker0@gmail.com>
2020-09-24 22:12:57 +0530

    Get and set for String

---
+++ src/stringTransfer.java
@@ -0,0 +1,11 @@
+public class stringTransfer {
+    public String name;
+
+    public void setName(String i){
+        System.out.println(name + " was set to " + i);
+        this.name = i;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
